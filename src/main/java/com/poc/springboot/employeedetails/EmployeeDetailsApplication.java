06bf392f8a4d0f4e89dbe2878f7f94e760aeeea6
abhijit.panchal <abhijit.panchal@accenture.com>
2017-02-20 15:15:16 +0530

    initial commit

--- src/main/java/com/poc/springboot/employeedetails/EmployeeDetailsApplication.java
+++ src/main/java/com/poc/springboot/employeedetails/EmployeeDetailsApplication.java
@@ -1,13 +1,8 @@
 package com.poc.springboot.employeedetails;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmployeeDetailsApplication {
